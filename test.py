c51c74347d53e82affc355df2b8d3b0996f42bd7
Tag143 <jtw93843@uga.edu>
2023-02-08 13:22:02 -0500

    Starting moment

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 import cv2
 from PIL import Image, ImageTk
+import threading
+import mediapipe as mp
+mp_dr = mp.solutions.drawing_utils
+mp_drst = mp.solutions.drawing_styles
+mp_h = mp.solutions.holistic
 
 # Create the GUI window
 root = tk.Tk()
@@ -14,7 +19,9 @@ def webcam_toggle():
         label.config(image="")
     else:
         cap = cv2.VideoCapture(0)
-        update_frame()
+        # x = threading.Thread(target=media, args=())
+        # x.start()
+        media()
 
 # Define a function to update the webcam output
 def update_frame():
@@ -26,6 +33,46 @@ def update_frame():
     label.config(image=img)
     label.image = img
     root.after(30, update_frame)
+
+def media():
+    cap = cv2.VideoCapture(0)
+    with mp_h.Holistic(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as holistic:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
+
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = holistic.process(image)
+
+            # Draw landmark annotation on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            mp_dr.draw_landmarks(
+                image,
+                results.face_landmarks,
+                mp_h.FACEMESH_CONTOURS,
+                landmark_drawing_spec=None,
+                connection_drawing_spec=mp_drst
+                .get_default_face_mesh_contours_style())
+            mp_dr.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_h.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drst
+                .get_default_pose_landmarks_style())
+            # Flip the image horizontally for a selfie-view display.
+            cv2.imshow('MediaPipe Holistic', cv2.flip(image, 1))
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+    cap.release()
 
 # Create a button to control the webcam
 button = tk.Button(root, text="Turn On/Off Webcam", command=webcam_toggle)
